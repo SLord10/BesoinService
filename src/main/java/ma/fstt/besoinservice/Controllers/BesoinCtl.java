68d99ef4162971330eb422488d1c93a5f8e43c5e
Da-achraf <achraf.boukir01@gmail.com>
2024-01-07 18:21:38 +0100

    Parsing Response Body

--- src/main/java/ma/fstt/besoinservice/Controllers/BesoinCtl.java
+++ src/main/java/ma/fstt/besoinservice/Controllers/BesoinCtl.java
@@ -4,9 +4,12 @@ import ma.fstt.besoinservice.Entities.Besoin;
 import ma.fstt.besoinservice.Services.BesoinService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RequestMapping("/besoin")
 @RestController
@@ -16,38 +19,67 @@ public class BesoinCtl {
     BesoinService besoinService;
 
     @PostMapping("")
-    public String add(@RequestBody Besoin Besoin){
+    public ResponseEntity<Map<String, Object>> add(@RequestBody Besoin Besoin){
         besoinService.save(Besoin);
-        return "saved";
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", "saved");
+        return ResponseEntity.ok(response);
     }
 
     @DeleteMapping("/{id}")
-    public String delete(@PathVariable("id") Long id){
+    public ResponseEntity<Map<String, Object>> delete(@PathVariable("id") Long id){
         besoinService.delete(id);
-        return "deleted";
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", "deleted");
+        return ResponseEntity.ok(response);
     }
 
-    @GetMapping("")
-    public List<Besoin> getall(){
-        return besoinService.getall();
+    @GetMapping
+    public ResponseEntity<Map<String, Object>> getall(){
+        List<Besoin> besoins = besoinService.getall();
+        Map<String, Object> response = new HashMap<>();
+        if (!besoins.isEmpty()){
+            response.put("data", besoins);
+            return ResponseEntity.ok(response);
+        }else {
+            response.put("data", null);
+            return ResponseEntity.ok(response);
+        }
     }
 
     @GetMapping("/{id}")
-    public Besoin getone(@PathVariable("id") Long id ){
-        return besoinService.getone(id);
+    public ResponseEntity<Map<String, Object>> getone(@PathVariable("id") Long id ){
+        Besoin Besoin = besoinService.getone(id);
+        Map<String, Object> response = new HashMap<>();
+        if (Besoin != null){
+            response.put("data", Besoin);
+            return ResponseEntity.ok(response);
+        }else {
+            response.put("data", null);
+            return ResponseEntity.ok(response);
+        }
     }
 
     @PatchMapping("/{id}")
-    public String update(@PathVariable("id") Long id ,@RequestBody Besoin Besoin) {
+    public ResponseEntity<Map<String, Object>> update(@PathVariable("id") Long id ,@RequestBody Besoin Besoin) {
         Besoin.setBesoin_id(id);
         besoinService.update(Besoin);
-        return "updated";
-
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", "updated");
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/user/{id_user}")
-    public List<Besoin> findByUser_id(@PathVariable("id_user") Long id_user ){
-        return besoinService.findByUser_id(id_user);
+    public ResponseEntity<Map<String, Object>> findByUser_id(@PathVariable("id_user") Long id_user ){
+        List<Besoin> besoins = besoinService.findByUser_id(id_user);
+        Map<String, Object> response = new HashMap<>();
+        if (!besoins.isEmpty()){
+            response.put("data", besoins);
+            return ResponseEntity.ok(response);
+        }else {
+            response.put("data", null);
+            return ResponseEntity.ok(response);
+        }
     }
 
 }
