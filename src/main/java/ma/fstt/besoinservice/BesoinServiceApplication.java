ef66571bb5e230bda420d7645fe0f3fec8998857
slord10 <soufianelamribah2000@gmail.com>
2025-01-20 14:24:45 +0100

    20/02

--- src/main/java/ma/fstt/besoinservice/BesoinServiceApplication.java
+++ src/main/java/ma/fstt/besoinservice/BesoinServiceApplication.java
@@ -2,8 +2,11 @@ package ma.fstt.besoinservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BesoinServiceApplication {
 
     public static void main(String[] args) {
